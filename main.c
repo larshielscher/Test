577777dc418106a290f60da9c74be85aab5ba0eb
Lars Hielscher <lars-hielscher@web.de>
2020-06-06 09:51:33 +0200

    sub fcn

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 # include <stdio.h>
+#include "sub.h"
 
 int main(){
     printf("this is my calculator\n");
     
+    double x = 3;
+    double y = 2;
+    printf("%lf - %lf = %lf", x, y, sub(x,y));
 
 }
---
+++ sub.c
@@ -0,0 +1,3 @@
+double sub(double x1, double x2){
+    return (x1 - x2);
+}
